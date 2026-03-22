587f5b1656f4a34044a8c06ee60bda21ac9e2262
Anshika Gupta <anshikagupta1704@gmail.com>
2026-03-23 04:44:49 +0530

    Adding some changes

---
+++ main.js
@@ -0,0 +1,27 @@
+const state = {
+  tool: null,
+  stroke: "#000000",
+  fill: "#000000",
+  border: "#000000",
+  size: 5,
+  opacity: 1,
+
+  objects: [],
+  history: [],
+  selected: null
+};
+
+const buttons = document.querySelectorAll(".tools button");
+function Clicked(event) {
+    let ClickedButton = event.current;
+    state.tool = ClickedButton.id;
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove("active");
+    }
+    ClickedButton.classList.add("active");
+    Sidebar(state.tool);
+}
+
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", Clicked);
+}
